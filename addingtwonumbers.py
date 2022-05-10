f75b3aa6c65ebb0a00c0c1d4289dd9db2bfb422f
aparnaks <aparna.ks@mintnblack.com>
2022-05-10 14:29:20 +0530

    adding two numbers

---
+++ addingtwonumbers.py
@@ -0,0 +1,10 @@
+# adding two numbers
+
+# defining a function
+def sum(a,b):
+    # adding that values in to a varible c
+    c = a + b
+    # printing the output
+    print(a, "+", b, "=", c)
+# calling that variable
+sum(3,4)
